918687386d357a7e9489b3f6d5d894c5dd989c32
snizio <cristianociaccio@gmail.com>
2023-01-31 12:59:42 +0100

    lol

--- preprocessing.py
+++ preprocessing.py
@@ -6,6 +6,11 @@ from nltk.corpus import wordnet
 import re
 from gensim.models import Word2Vec
 from nltk.tokenize.treebank import TreebankWordDetokenizer
+from gensim.models import KeyedVectors
+
+# print("loading pre trained embeddings, this can take some minutes...")
+# glove_twitter_25 = KeyedVectors.load_word2vec_format('glove-twitter-25.txt', binary=False)
+# print("loading complete.")
 
 custom_stopwords = ['echo', 'alexa', "alexia", 'dot', "star", 'amazon', 'prime', '2nd', 'generation', "fire", "stick", "firestick", "skype", "facetime", '1st', '3rd', '4th', '5th', "hub", "hulu", 'google', 'netflix', "sony", 'youtube', 'philip', 'tp-link', 'fourth', 'roku', "siri", 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'nor', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "...", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', 'wasn', 'weren', 'won', 'wouldn']
 pos_list = ["JJ", "JJR", "JJS", "RB", "RBR", "RBS", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"] # lista di pos utilizzate per eventualmente filtrare durante la tokenizzazione
@@ -16,8 +21,9 @@ def negation_handler(sentence):
 
     temp = int(0)
     for i in range(len(sentence)):
-        if sentence[i-1] in ['not',"n't", "no", "without"]:
+        if sentence[i-1] in ['not',"n't", "no", "without", "t"]:
             antonyms = []
+            antonym_found = False
             for syn in wordnet.synsets(sentence[i]):
                 syns = wordnet.synsets(sentence[i])
                 w1 = syns[0].name()
@@ -40,30 +46,15 @@ def negation_handler(sentence):
                         antonym_max = ant
                         sentence[i] = antonym_max
                         sentence[i-1] = ''
+                        antonym_found = True
+            if not antonym_found:
+                sentence[i] = f"{sentence[i-1]}_{sentence[i]}"
+                sentence[i-1] = ''
+
     while '' in sentence:
         sentence.remove('')
     return sentence
 
-
-
-def undersample_positive(dataset):
-
-    """Bilancia il dataset uniformando il numero di recensioni negative e positive. Prende in input il dataset e ritorna il dataset bilanciato."""
-
-    reviews_1 = list(dataset[dataset["feedback"] == 1]["verified_reviews"])
-    reviews_0 = list(dataset[dataset["feedback"] == 0]["verified_reviews"])
-    reviews_1.sort(key=len, reverse = True)
-    sample_1 = reviews_1[:len(reviews_0)]
-    verified_reviews_sample = []
-    feedback_sample = []
-    verified_reviews_sample.extend(sample_1)
-    verified_reviews_sample.extend(reviews_0)
-    feedback_sample.extend([1 for i in range(len(sample_1))])
-    feedback_sample.extend([0 for i in range(len(reviews_0))])
-    dataset = pd.DataFrame({"verified_reviews":verified_reviews_sample, "feedback": feedback_sample})
-    print(dataset["feedback"].value_counts())
-
-    return dataset
 
 
 def get_wordnet_pos(treebank_tag):
@@ -84,8 +75,8 @@ def get_wordnet_pos(treebank_tag):
         return "n"
         
 
-def tokenize_list_of_text(list_of_text, custom_stopwords = [], pos_filter = False, pos_list = []):
-    """Tokenizza tutte le recensioni, pulisce da stopwords, elimina token <= 2 caratteri e lemmatizza.
+def tokenize_list_of_text(list_of_text, custom_stopwords = [], pos_filter = False, pos_list = [], token_len = 2):
+    """Tokenizza tutte le recensioni, pulisce da stopwords, elimina token <= token_len caratteri e lemmatizza.
     Ritorna sia il la lista tokenizzata ma come stringa sia come lista di tokens, dunque ritorna due elementi"""
 
     lemmatizer = nltk.WordNetLemmatizer()
@@ -95,14 +86,15 @@ def tokenize_list_of_text(list_of_text, custom_stopwords = [], pos_filter = Fals
     sent_tokenized_reviews = []
     for review in list_of_text: #pulisce le recensioni
         review = re.sub(r'\d+', '', review) # elimina i numeri
+        review = re.sub(r'[^\w\s\']+', ' ', review) # sostituisce la punteggiatura con uno spazio
         tokens = nltk.tokenize.word_tokenize(review, language='english', preserve_line=False) # tokenizza
         tokens = [w.lower() for w in tokens] # mette in minuscolo
         tokens_pos = pos_tag(tokens) # pos tagging
         lemmatized_tokens = [(lemmatizer.lemmatize(w, get_wordnet_pos(pos)), pos) for w, pos in tokens_pos] # lemmatizza in base al pos tag
         if pos_filter: # se si vuole filtrare per la lista di pos
-            clean_tokens = [w for w, pos in lemmatized_tokens if w not in string.punctuation and len(w)>2 and w not in custom_stopwords and pos in pos_list]
+            clean_tokens = [w for w, pos in lemmatized_tokens if w not in string.punctuation and len(w)>token_len and w not in custom_stopwords and pos in pos_list]
         else:
-            clean_tokens = [w for w, pos in lemmatized_tokens if w not in string.punctuation and len(w)>2 and w not in custom_stopwords]
+            clean_tokens = [w for w, pos in lemmatized_tokens if w not in string.punctuation and len(w)>token_len and w not in custom_stopwords]
         clean_tokens = negation_handler(clean_tokens) # gestisce le negazioni, vedi funzione
         sent_tokenized_reviews.append(clean_tokens)
         tokenized_reviews.append(detokenizer.detokenize(clean_tokens))
@@ -116,7 +108,7 @@ def tokenize_list_of_text(list_of_text, custom_stopwords = [], pos_filter = Fals
 
     return tokenized_reviews,  sent_tokenized_reviews # ritorna una tupla, il primo elemento contiene le recensioni in formato stringa, il secondo le continene tokenizzate
 
-def generate_samples(reviews = [], n = 150):
+def generate_samples(reviews, n = 150, pre_trained_model = None):
 
     """Genera recensioni sintetiche. Ciò avviene sostituendo i token delle recensioni in input con i sinonimi più coerenti."
     Prende in input le recensioni tokenizzate e il numero di recensioni da generare (deve essere minore del numero di recensioni in input).
@@ -128,9 +120,12 @@ def generate_samples(reviews = [], n = 150):
     random.seed(10)
     reviews_to_sample = random.sample(reviews, n) # si fa un sample di n recensioni
     generated_reviews = []
-    # si addestra w2v solo sulle recensioni di input alla funzione
-    w2v_model_neg = Word2Vec(reviews, vector_size=300, window = 5, min_count = 0, sg=1, hs = 1, alpha=0.03, min_alpha=0.0007, seed = 5)
-    w2v_model_neg.train(reviews, total_examples=len(reviews), epochs=100)
+
+    # # si addestra w2v solo sulle recensioni di input alla funzione
+    if not pre_trained_model:
+        w2v_model_neg = Word2Vec(reviews, vector_size=100, window = 10, min_count = 0, sg=1, hs = 1, epochs= 100, seed = 5).wv
+    else:
+        w2v_model_neg = pre_trained_model
 
     for review in reviews_to_sample:
         new_review = []
@@ -145,32 +140,42 @@ def generate_samples(reviews = [], n = 150):
             for word_sim in synonyms:
                 if word_sim != w and word_sim != "" and "_" not in word_sim:
                     try:
-                        score = w2v_model_neg.wv.similarity(w, word_sim)
+                        score = w2v_model_neg.similarity(w, word_sim)
                         sim_score[word_sim] = score
                     except KeyError:
                         continue
             
             if sim_score:
                 new_review.append(max(sim_score, key=sim_score.get)) #si prende il sinonimo col punteggio maggiore
-            else:
-                new_review.append(w)
 
-        if new_review:
+        if new_review and new_review != review:
+            print(review)
+            print("-")
+            print(new_review)
+            print("--------------------------")
             generated_reviews.append(new_review)
 
     return generated_reviews
 
-def drop_positive_duplicates(dataset):
+def frequency_cleaning(new_sent_tok, n):
+    
+    tot_tokens = []
 
-    """Droppa i duplicati delle recensioni positive, insieme alle foto di agosto..."""
-        
-    df_no_duplicates = dataset.drop_duplicates(subset="verified_reviews")
-    index_positive = list(dataset[dataset["feedback"] == 1].index)
-    dataset = pd.concat([dataset.drop(labels=index_positive), df_no_duplicates])
+    for sent in new_sent_tok:
+        for tok in sent:
+            tot_tokens.append(tok)
 
-    return dataset
+    freqs = nltk.FreqDist(tot_tokens)
+    cleaned_reviews = []
 
+    for sent in new_sent_tok:
+        clean_sent = []
+        for tok in sent:
+            if freqs[tok] > n:
+                clean_sent.append(tok)
+        cleaned_reviews.append(clean_sent)
 
+    return cleaned_reviews
 
 
 # La pipeline standard:
